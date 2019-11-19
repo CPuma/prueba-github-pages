249fce3a37b9f9014962715f535360b8c1d02f09
Cesar Puma <rasec@pop-os.localdomain>
2019-11-19 08:41:50 -0500

    problema con GRID y el width 100%_ antes de remover GRID del HOME

---
+++ js/splide_home.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function() {
+  // ===========CARDSS=================
+  let cardsSlider = new Splide("#card-slider", {
+    perPage: 3,
+    pagination: false,
+    height: 350,
+    cover: true,
+    focus: true,
+    lazyload: true,
+    // width: '100vw',
+    breakpoints: {
+      "750": { perPage: 1 },
+      "1070": { perPage: 2 }
+    }
+  }).mount();
+});
