03f9c3c2c90034c8126abe5b95c3fad6a211617a
Cesar Puma <rasec@pop-os.localdomain>
2019-11-22 11:20:34 -0500

    antes de cambiar los filtros

---
+++ js/buscador.js
@@ -0,0 +1,57 @@
+// CHECK BOXES del FILTRO
+function selectModeda(id) {
+  for (var i = 1; i <= 2; i++) {
+    document.getElementById('chMoneda' + i).checked = false;
+  }
+  document.getElementById(id).checked = true;
+}
+
+function selectBaños(id) {
+  for (var i = 1; i <= 4; i++) {
+    document.getElementById('chBaño' + i).checked = false;
+  }
+  document.getElementById(id).checked = true;
+}
+function selectDormitorios(id) {
+  for (var i = 1; i <= 4; i++) {
+    document.getElementById('chDormitorio' + i).checked = false;
+  }
+  document.getElementById(id).checked = true;
+}
+// =====================================
+
+// ==========OCULTANDO FILTROS=========
+let filtro = document.getElementById('filtro');
+
+let ocultarFiltro = function() {
+  if (window.innerWidth < 850) {
+    filtro.classList.add('pushbar');
+    filtro.classList.add('from_left');
+  } else {
+    filtro.classList.remove('pushbar');
+    filtro.classList.remove('from_left');
+  }
+};
+
+ocultarFiltro();
+
+window.addEventListener('resize', function(e) {
+  ocultarFiltro();
+});
+// =====================================00
+
+//=========  INSTANCIANDO  SLIDERS ==========
+let inmublesSplides = document.getElementsByClassName('inmueble_splide');
+for (let i = 0; i < inmublesSplides.length; i++) {
+  let id = '#' + inmublesSplides[i].id;
+  document.addEventListener('DOMContentLoaded', function() {
+    // new Splide('#image-slider', {
+    new Splide(id, {
+      cover: true,
+      pagination: true,
+      focus: 'center',
+      width: '105%',
+      trimSpace: false
+    }).mount();
+  });
+}
